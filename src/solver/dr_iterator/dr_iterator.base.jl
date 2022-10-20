9f7b39c23de1a926cf1ec6dec121465d7fcb7683
neoabs <mateusz.szetela@pm.me>
2022-10-21 00:11:30 +0200

    sfd

--- src/solver/dr_iterator/dr_iterator.base.jl
+++ src/solver/dr_iterator/dr_iterator.base.jl
@@ -28,7 +28,7 @@ function iterate_decission_rules(mdl::Model)
         old_vf, old_dr, old_vfalter = copy(mdl.VF), copy(mdl.DR), copy(mdl.VFalter)
         
         #find optimal decission rules/ vf matrix given old DR / old VF
-        mdl = dr_CPU(mdl)
+        mdl = copy(dr_CPU(mdl))
         
         #optional afterprocessing
         mdl.VF_afterprocess(mdl)
